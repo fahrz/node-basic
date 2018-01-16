e7b2bf4cbb1010b0647e5ff4f9f293d3d8a5fb7d
fahrz <rezatangse@gmail.com>
2018-01-16 13:53:45 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', function(req, res){
-  res.json({message: "Selamat datang di NodeJS!"});
+  res.json({message: "Selamat datang di NodeJS"});
 });
 
 router.get('/users', function(req, res){
